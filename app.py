1a97cee1ba27ed865ca79e8dc34e9495f497f553
Matěj Přibyl <matej.pribyl.official@gmail.com>
2026-08-03 21:10:21 +0200

    add secure api connection
    added api connection to display content from NASA

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.title("Space Birthday", width = "stretch", text_alignment = "center")
 st.subheader("Welcome to the Space Birthday app, where you can see what happened on your birthday in space history!")
@@ -6,4 +7,22 @@ st.subheader("Welcome to the Space Birthday app, where you can see what happened
 birth_date = st.date_input("Enter your birthday date:")
 left, middle, right = st.columns(3)
 if middle.button("Submit", icon="🚀", width="stretch"):
-    st.write(f"Your birthday is: {birth_date}")
+    api_key = st.secrets["NASA_API_KEY"]
+    url = f"https://api.nasa.gov/planetary/apod?date={birth_date}&api_key={api_key}"
+    response = requests.get(url)
+    data = response.json()
+
+    title = data["title"]
+    image = data["url"]
+    explanation = data["explanation"]
+    media_type = data["media_type"]
+
+
+    st.header(title)
+    if media_type == "image":
+        st.image(image)
+    elif media_type == "video":
+        st.video(image)
+    else:
+        st.write("Media type not supported.")
+    st.write(explanation)
