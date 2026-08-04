a8165f66b3721792c354c03acb2c0cae19ce6716
Matěj Přibyl <matej.pribyl.official@gmail.com>
2026-08-04 13:30:37 +0200

    last UI enhance before shipment
    improved the app design

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import streamlit as st
 import requests
 import datetime
 
+st.set_page_config(page_title="Space Birthday", page_icon="🚀")
 st.title("Space Birthday", width = "stretch", text_alignment = "center")
-st.subheader("Welcome to the Space Birthday app, where you can see what happened on your birthday in space history!")
+st.subheader("Explore the NASA APOD on your birthday! 🌌", text_alignment= "center")
 
 earlier_date = datetime.date(1995, 6, 16)
 today = datetime.date.today()
-birth_date = st.date_input("Enter your birthday date:", min_value=earlier_date, max_value=today)
 left, middle, right = st.columns(3)
+birth_date = middle.date_input("Enter your birthday date:", min_value=earlier_date, max_value=today, width = 400)
 if middle.button("Submit", icon="🚀", width="stretch"):
     api_key = st.secrets["NASA_API_KEY"]
     url = f"https://api.nasa.gov/planetary/apod?date={birth_date}&api_key={api_key}"
@@ -20,12 +21,13 @@ if middle.button("Submit", icon="🚀", width="stretch"):
     explanation = data["explanation"]
     media_type = data["media_type"]
 
-
+    st.divider()
     st.header(title)
     if media_type == "image":
-        st.image(image)
+        st.image(image, use_container_width=True)
     elif media_type == "video":
         st.video(image)
     else:
         st.write("Media type not supported.")
-    st.write(explanation)
+    with st.expander("Read NASA's explanation:"):
+        st.write(explanation)
