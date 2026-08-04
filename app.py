bf62535ff27dc75bed346155dc328241e3ae5d0e
Matěj Přibyl <matej.pribyl.official@gmail.com>
2026-08-04 12:14:09 +0200

    fix calendar input
    fixed the earliest and maximum date to select in the calendar

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import requests
+import datetime
 
 st.title("Space Birthday", width = "stretch", text_alignment = "center")
 st.subheader("Welcome to the Space Birthday app, where you can see what happened on your birthday in space history!")
 
-birth_date = st.date_input("Enter your birthday date:")
+earlier_date = datetime.date(1995, 6, 16)
+today = datetime.date.today()
+birth_date = st.date_input("Enter your birthday date:", min_value=earlier_date, max_value=today)
 left, middle, right = st.columns(3)
 if middle.button("Submit", icon="🚀", width="stretch"):
     api_key = st.secrets["NASA_API_KEY"]
